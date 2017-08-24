42be2ed872118a4f6b41a251a2aebc6d8fd71039
xiyuan_fengyu <xiyuan_fengyu@163.com>
2017-08-24 11:58:02 +0800

    md5 InputStream File 计算

--- src/main/java/com/xiyuan/util/Md5Util.java
+++ src/main/java/com/xiyuan/util/Md5Util.java
@@ -1,13 +1,18 @@
 package com.xiyuan.util;
 
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.ArrayList;
 
 /**
  * Created by xiyuan_fengyu on 2016/8/26.
  */
 public class Md5Util {
+
+    private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
 
     private static String byteArrayToHexString(byte bs[]) {
         StringBuilder resultSb = new StringBuilder();
@@ -32,9 +37,9 @@ public class Md5Util {
 
     public static String get(String origin, Charset charsets) {
         if (charsets == null) {
-            return get(origin.getBytes());
+            charsets = StandardCharsets.UTF_8;
         }
-        else return get(origin.getBytes(charsets));
+        return get(origin.getBytes(charsets));
     }
 
     public static String get(byte[] origin) {
@@ -48,7 +53,87 @@ public class Md5Util {
         return resultString;
     }
 
-    private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+    public static String get(InputStream in) {
+        if (in == null) return "";
+
+        String resultString = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            updateDigest(md, in);
+            resultString = byteArrayToHexString(md.digest());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultString;
+    }
+
+    private static void updateDigest(MessageDigest md, InputStream in) throws IOException {
+        byte[] buffer = new byte[2048];
+        int len;
+        while ((len = in.read(buffer)) != -1) {
+            md.update(buffer, 0, len);
+        }
+    }
+
+    public static String get(File file) {
+        if (file != null && file.exists()) {
+            if (file.isFile()) {
+                return getSingleFile(file);
+            }
+            else {
+                ArrayList<File> subs = new ArrayList<>();
+                subFiles(file, subs);
+                try {
+                    MessageDigest md = MessageDigest.getInstance("MD5");
+                    for (File sub : subs) {
+                        md.update(getRelativePath(sub, file).getBytes(StandardCharsets.UTF_8));
+                        updateDigest(md, new FileInputStream(sub));
+                    }
+                    return byteArrayToHexString(md.digest());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "";
+    }
+
+    private static String getRelativePath(File cur, File root) {
+        String path = cur.getAbsolutePath().substring(root.getAbsolutePath().length()).replace('\\', '/');
+        while (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        return path;
+    }
+
+    private static String getSingleFile(File file) {
+        try {
+            return get(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static void subFiles(File root, ArrayList<File> result) {
+        File[] files = root.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    result.add(file);
+                }
+                else {
+                    subFiles(file, result);
+                }
+            }
+        }
+    }
 
 }
