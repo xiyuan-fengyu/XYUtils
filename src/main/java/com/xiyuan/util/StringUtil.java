8722f14e8151460b7ab0b008c38b03dd169a3387
xiyuan_fengyu <xiyuan_fengyu@163.com>
2017-11-13 11:30:30 +0800

    增加 随机字符串方法

--- src/main/java/com/xiyuan/util/StringUtil.java
+++ src/main/java/com/xiyuan/util/StringUtil.java
@@ -61,5 +61,32 @@ public class StringUtil {
         return max;
     }
 
+    private static final char[] chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public static String randomStr(int len) {
+        if (len <= 0) return "";
+
+        char[] randChars = new char[len];
+        for (int i = 0; i < len; i++) {
+            randChars[i] = chars[(int) (Math.random() * chars.length)];
+        }
+        return String.valueOf(randChars);
+    }
+
+    public static String randomStrWithTime(int len, String divider) {
+        StringBuilder builder = new StringBuilder();
+        int charsLen = chars.length;
+        for (int i = 0; i < len; i++) {
+            builder.append(chars[(int) (Math.random() * charsLen)]);
+        }
+        builder.append(divider);
+
+        long now = System.currentTimeMillis();
+        while (now > 0) {
+            builder.append(chars[(int) (now % charsLen)]);
+            now /= charsLen;
+        }
+        return builder.toString();
+    }
 
 }
